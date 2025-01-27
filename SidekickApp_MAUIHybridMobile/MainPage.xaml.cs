5f47b012dc765fc1ed814ab854126e8527aa2697
JeremyJalpha <jeremyjalpha@gmail.com>
2025-01-27 08:20:57 +0200

    Add project files.

---
+++ SidekickApp_MAUIHybridMobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SidekickApp_MAUIHybridMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SidekickApp_MAUIHybridMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SidekickApp_MAUIHybridMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
